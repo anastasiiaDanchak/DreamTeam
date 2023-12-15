73229ddd9fa36f87fdc17c6cd9612d49781935e6
Taras Cherkas <128377317+MischievousPloy@users.noreply.github.com>
2023-12-15 22:59:09 +0200

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -57,3 +57,58 @@ void MainWindow::saveBoard()
     logTextEdit->append("Saving board...");
     closeBoard();
 }
+
+// Input Output Function 
+
+readIdeasFromFile("ideas.txt");
+voteForIdeas();
+writeIdeasToFile("ideas.txt");
+saveBoard();
+
+void MainWindow::readIdeasFromFile(const QString &fileName)
+{
+    logTextEdit->append("Reading ideas from file...");
+
+    QFile file(fileName);
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QTextStream stream(&file);
+        while (!stream.atEnd())
+        {
+            QString idea = stream.readLine();
+            ideas.append(idea);
+        }
+        file.close();
+
+        logTextEdit->append("Ideas read successfully.");
+    }
+    else
+    {
+        logTextEdit->append("Error: Unable to open the file for reading.");
+    }
+}
+
+void MainWindow::writeIdeasToFile(const QString &fileName)
+{
+    logTextEdit->append("Writing ideas to file...");
+
+    QFile file(fileName);
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text))
+    {
+        QTextStream stream(&file);
+        for (const QString &idea : ideas)
+        {
+            stream << idea << Qt::endl;
+        }
+        file.close();
+
+        logTextEdit->append("Ideas written to file successfully.");
+    }
+    else
+    {
+        logTextEdit->append("Error: Unable to open the file for writing.");
+    }
+}
+
+// Input Output Function  END
+
